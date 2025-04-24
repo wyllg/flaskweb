118a6c6e21bd1e084d2f1fd52094851d5bcaad02
wyllg <23-01591@g.batstate-u.edu.ph>
2025-04-24 09:50:38 +0800

    Navbar and Extends and Base

--- website/views.py
+++ website/views.py
@@ -6,10 +6,10 @@ views = Blueprint('views', __name__)
 def home():
     return render_template('home.html')
 
-@views.route('about')
+@views.route('/about')
 def about():
     return render_template('about.html')
 
-@views.route('contact')
+@views.route('/contact')
 def contact():
     return render_template('contact.html')
